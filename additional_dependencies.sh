22be8a01693069e8245321d9a4b85ae7e6e3813d
Romeo Cozac <romeo_cozac@yahoo.com>
2020-11-10 13:55:31 +0900

    updating environment and configs

---
+++ additional_dependencies.sh
@@ -0,0 +1,5 @@
+pip install torch-scatter==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
+pip install torch-sparse==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
+pip install torch-cluster==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
+pip install torch-spline-conv==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
+pip install torch-geometric
