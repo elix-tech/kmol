768503a11634a9179035581bb13076f1342f6637
Romeo Cozac <romeo_cozac@yahoo.com>
2021-06-02 14:14:27 +0900

    [bugfix] Explicitly specify the torch geometric version

--- install_additional_dependencies.sh
+++ install_additional_dependencies.sh
@@ -2,4 +2,4 @@ pip install torch-scatter==latest+cu102 -f https://pytorch-geometric.com/whl/tor
 pip install torch-sparse==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
 pip install torch-cluster==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
 pip install torch-spline-conv==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
-pip install torch-geometric
+pip install torch-geometric==1.6.0
