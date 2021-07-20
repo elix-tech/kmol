67dd679f4b122189551785d6161e9fe73483bc8d
Romeo Cozac <romeo_cozac@yahoo.com>
2021-07-20 15:14:32 +0900

    [bugfix] Fixing dictionary cloning issues

--- lib/core/config.py
+++ lib/core/config.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os
 from collections import defaultdict
+from copy import deepcopy
 from dataclasses import dataclass, field
 from typing import Literal, Optional, List, Dict, Any, DefaultDict
 
@@ -83,7 +84,7 @@ class Config:
             DifferentialPrivacy.setup(**self.differential_privacy["options"])
 
     def cloned_update(self, **kwargs) -> "Config":
-        options = vars(self)
+        options = deepcopy(vars(self))
         options.update(**kwargs)
 
         return Config(**options)
