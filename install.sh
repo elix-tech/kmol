f43e9a92088d6799b6baaead66dd44b50001be31
vincrichard <55540832+vincrichard@users.noreply.github.com>
2022-09-07 11:26:49 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ pip install torch-sparse==latest+cu102 -f https://pytorch-geometric.com/whl/torc
 pip install torch-cluster==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html --use-deprecated=legacy-resolver
 pip install torch-spline-conv==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html --use-deprecated=legacy-resolver
 
-pip install torch-geometric==1.6.0
+pip install torch-geometric==1.6.3
 pip install -e .
