efc7046ece91856f076ecf7192dae30c8501dc6f
vincrichard <vincent.richard@elix-inc.com>
2023-10-03 14:25:49 +0900

    [Add] Protein captum atribution to pdb files

--- src/kmol/script/protein_captum.py
+++ src/kmol/script/protein_captum.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import torch
 from tqdm import tqdm
 import numpy as np
+from openbabel import pybel
 
 from ..model.architectures import ProteinSchnetNetwork
 from .integrated_gradient import CaptumScript, GRAPH_CAPTUM_FEAT
@@ -10,51 +11,38 @@ from .integrated_gradient import CaptumScript, GRAPH_CAPTUM_FEAT
 PROTEIN_FEATURES = ["protein", "schnet_input"]
 GRAPH_CAPTUM_FEAT += ["z", "z_protein", "coords", "edge_attr"]
 
-MODEL_ATTRIBUTION = {
-    ProteinSchnetNetwork: {
-        "type": "captum.attr.LayerIntegratedGradients",
-        "layer": ["embedding.0", "protein_embedding.0", "protein_embedding.1", "interactions.0.mlp_protein"],
-    }
-}
-
 
 class ProteinCaptumScript(CaptumScript):
-    def __init__(self, config_path, protein_input_name, processing_type, n_steps: int = 50) -> None:
+    def __init__(self, config_path, n_steps: int = 50) -> None:
         super().__init__({}, config_path, "sum", n_steps)
         self.attribution, groups = self.get_integrated_layer_protein_schnet(self.model)
         self.attribution_innit()
-        self.protein_input_name = protein_input_name
-        self._process = getattr(self, f"process_{processing_type}")
         self.groups = groups
 
     def run(self):
-        protein_inputs = self.dataset[self.protein_input_name].values.tolist()
-        for protein_raw_data, data in tqdm(zip(protein_inputs, self.data_loader)):
+        for data in tqdm(self.data_loader):
             self.tmp_executor._to_device(data)
             attributions = []
-            attribute_result = self.attributor.attribute(data.inputs, target=0, n_steps=self.n_steps)
+            target = self.model(data.inputs).argmax(axis=1).detach().cpu().item()
+            attribute_result = self.attributor.attribute(data.inputs, target=target, n_steps=self.n_steps)
             results = []
             for i in range(len(self.groups) - 1):
                 result = torch.concat(attribute_result[self.groups[i] : self.groups[i + 1]], axis=1).sum(axis=1)
                 results.append(result.detach().cpu().numpy())
             ligand_atom, protein_atom, lp_interaction = results
-            atoms = np.concatenate([ligand_atom, protein_atom])
+            attributions = np.concatenate([ligand_atom, protein_atom])
             edge_index = data.inputs["schnet_inputs"].edge_index.detach().cpu().numpy()
             for i in range(len(lp_interaction)):
                 src, dest = edge_index[0, i], edge_index[1, i]
-                atoms[src] = atoms[src] + lp_interaction[i] / 2
-                atoms[dest] = atoms[dest] + lp_interaction[i] / 2
-            # TODO Add conversion to original atoms and add to files.
+                attributions[src] = attributions[src] + lp_interaction[i] / 2
+                attributions[dest] = attributions[dest] + lp_interaction[i] / 2
             # attributions.append(attribution[0].detach().cpu().numpy())
 
-            # target_sequence case
-            self._process(protein_raw_data, attributions)
-
-    def process_target_sequence(self, protein_raw_data, attributions):
-        result = ""
-        attributions = [a.squeeze() for a in attributions]
-        for data in enumerate(zip(protein_raw_data, *attributions)):
-            pass
+            self.generate_pdb(
+                mol2_path=data.inputs["schnet_inputs"].mol2_path[0],
+                attributions=attributions,
+                orinal_ids_mapping=data.inputs["schnet_inputs"].original_atom_ids.detach().cpu().numpy(),
+            )
 
     def get_integrated_layer_protein_schnet(self, model: ProteinSchnetNetwork):
         ligand_embedding = [f"embedding.{i}" for i in range(len(model.embedding))]
@@ -65,3 +53,24 @@ class ProteinCaptumScript(CaptumScript):
             "type": "captum.attr.LayerIntegratedGradients",
             "layer": ligand_embedding + protein_embedding + interaction_feature,
         }, groups
+
+    def generate_pdb(self, mol2_path, attributions, orinal_ids_mapping):
+        mol = next(pybel.readfile("mol2", mol2_path))
+        pdb_output = mol.write("pdb").splitlines()
+
+        # Modify PDB lines with new B-factor values
+        modified_pdb_output = []
+        for line in pdb_output:
+            if line.startswith("ATOM") or line.startswith("HETATM"):
+                atom_idx = int(line[6:11].strip()) - 1
+                mask = orinal_ids_mapping == atom_idx
+                if mask.any():
+                    line = line[:60] + f"{attributions[mask].item():6.2f}" + line[66:]
+
+            modified_pdb_output.append(line)
+
+        save_dir = Path(self._config.output_path) / "protein_captum"
+        save_dir.mkdir(exist_ok=True, parents=True)
+        pdb_file = save_dir / Path(mol2_path).with_suffix(".attribution.pdb").name
+        with open(pdb_file, "w") as f:
+            f.write("\n".join(modified_pdb_output))
